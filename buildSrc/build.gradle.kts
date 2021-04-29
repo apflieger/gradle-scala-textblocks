58239a07e49a5f425c6dbd49f6127466436d2916
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2021-04-29 18:59:11 +0200

    Copied sample project from https://github.com/apflieger/gradle-incremental-build

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    scala
+}
+repositories {
+    mavenCentral()
+}
+dependencies {
+    implementation("org.apache.commons:commons-text:1.6")
+}
---
+++ buildSrc/src/main/java/example/MyClass.java
@@ -0,0 +1,10 @@
+package example;
+
+import org.apache.commons.lang3.*;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        ArrayUtils.toArray();
+    }
+}
